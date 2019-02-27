d2279d5f26ad45d53e5e425ec213d8a7b72dedcf
Jizu Sun <jizu.sun@sap.com>
2019-02-27 22:15:27 +0800

    001_two-sum

---
+++ problems/001_two-sum/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hash = nums.reduce( (prev, current, idx) => {
+        prev[current] = idx;
+        return prev;
+    }, {})
+    
+    let result = [];
+    for (let i=0; result.length < 2; i++) {
+        const remain = target - nums[i];
+        const possibleIndex = hash[remain];
+        if (remain in hash && i !== possibleIndex) {
+            result = [i, possibleIndex]
+        }
+    }
+    return result;
+};
+
+module.exports = twoSum;
