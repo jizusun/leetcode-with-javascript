626da8ef05b73aea080f79710e295a67a1cb7769
Jizu Sun <sunjizu@gmail.com>
2019-05-16 23:48:53 +0800

    :art: add wrapper for running multiple solutions

--- problems/019_remove-nth-node-from-end-of-list/index.test.js
+++ problems/019_remove-nth-node-from-end-of-list/index.test.js
@@ -1,34 +1,31 @@
-const {LinkedList} = require('../../lib/LinkedList');
+const {LinkedList, run} = require('lib');
 const index = require('./index');
 
-Object.entries(index)
-  .forEach( ([name, removeNthFromEnd]) => {
+const runTests = (removeNthFromEnd) => {
 
-    describe(`${name}`, () => {
-
-      test('normal case', () => {
-        const linkedList = new LinkedList([1, 2, 3, 4, 5]);
-        const n = 2;
-        const actual = removeNthFromEnd(linkedList.head, n);
-        const expected = new LinkedList([1, 2, 3, 5]);
-        expect(actual).toEqual(expected.head);
-      });
-
-      test('remove the only node', () => {
-        const linkedList = new LinkedList([1]);
-        const n = 1;
-        const actual = removeNthFromEnd(linkedList.head, n);
-        const expected = new LinkedList([]);
-        expect(actual).toEqual(expected.head);
-      });
-
-      test('remove the actually first node', () => {
-        const linkedList = new LinkedList([1, 2, 3, 4, 5]);
-        const n = 5;
-        const actual = removeNthFromEnd(linkedList.head, n);
-        const expected = new LinkedList([2, 3, 4, 5]);
-        expect(actual).toEqual(expected.head);
-      });
-    });
+  test('normal case', () => {
+    const linkedList = new LinkedList([1, 2, 3, 4, 5]);
+    const n = 2;
+    const actual = removeNthFromEnd(linkedList.head, n);
+    const expected = new LinkedList([1, 2, 3, 5]);
+    expect(actual).toEqual(expected.head);
   });
 
+  test('remove the only node', () => {
+    const linkedList = new LinkedList([1]);
+    const n = 1;
+    const actual = removeNthFromEnd(linkedList.head, n);
+    const expected = new LinkedList([]);
+    expect(actual).toEqual(expected.head);
+  });
+
+  test('remove the actually first node', () => {
+    const linkedList = new LinkedList([1, 2, 3, 4, 5]);
+    const n = 5;
+    const actual = removeNthFromEnd(linkedList.head, n);
+    const expected = new LinkedList([2, 3, 4, 5]);
+    expect(actual).toEqual(expected.head);
+  });
+};
+
+run(index, runTests);
--- problems/206_reverse-linked-list/index.test.js
+++ problems/206_reverse-linked-list/index.test.js
@@ -1,23 +1,28 @@
-const {reverseList} = require('./index');
-const {LinkedList} = require('../../lib/LinkedList');
+const {LinkedList, run} = require('lib');
+const index = require('./index');
 
-it('normal case', ()=> {
-  const linkedList = new LinkedList([1, 2, 3, 4, 5]);
-  const expected = new LinkedList([5, 4, 3, 2, 1]);
-  const actual = reverseList(linkedList.head);
-  expect(actual).toEqual(expected.head);
-});
+const runTests = (reverseList) => {
 
-it('array with only one element', ()=> {
-  const linkedList = new LinkedList([1]);
-  const expected = new LinkedList([1]);
-  const actual = reverseList(linkedList.head);
-  expect(actual).toEqual(expected.head);
-});
+  it('normal case', ()=> {
+    const linkedList = new LinkedList([1, 2, 3, 4, 5]);
+    const expected = new LinkedList([5, 4, 3, 2, 1]);
+    const actual = reverseList(linkedList.head);
+    expect(actual).toEqual(expected.head);
+  });
 
-it('empty array', ()=> {
-  const linkedList = new LinkedList([]);
-  const expected = new LinkedList([]);
-  const actual = reverseList(linkedList.head);
-  expect(actual).toEqual(expected.head);
-});
+  it('array with only one element', ()=> {
+    const linkedList = new LinkedList([1]);
+    const expected = new LinkedList([1]);
+    const actual = reverseList(linkedList.head);
+    expect(actual).toEqual(expected.head);
+  });
+
+  it('empty array', ()=> {
+    const linkedList = new LinkedList([]);
+    const expected = new LinkedList([]);
+    const actual = reverseList(linkedList.head);
+    expect(actual).toEqual(expected.head);
+  });
+
+};
+run(index, runTests);
