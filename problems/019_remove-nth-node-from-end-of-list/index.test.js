4ac1214bb75c450377cae7180ab8c30780ce7069
Jizu Sun <sunjizu@gmail.com>
2019-05-16 21:08:29 +0800

    run test for different solutions

--- problems/019_remove-nth-node-from-end-of-list/index.test.js
+++ problems/019_remove-nth-node-from-end-of-list/index.test.js
@@ -1,26 +1,34 @@
-const { removeNthFromEndInOnePass: removeNthFromEnd } = require('./index');
 const {LinkedList} = require('../../lib/LinkedList');
+const index = require('./index');
 
-test('normal case', () => {
-  const linkedList = new LinkedList([1, 2, 3, 4, 5]);
-  const n = 2;
-  const actual = removeNthFromEnd(linkedList.head, n);
-  const expected = new LinkedList([1, 2, 3, 5]);
-  expect(actual).toEqual(expected.head);
-});
+Object.entries(index)
+  .forEach( ([name, removeNthFromEnd]) => {
 
-test('remove the only node', () => {
-  const linkedList = new LinkedList([1]);
-  const n = 1;
-  const actual = removeNthFromEnd(linkedList.head, n);
-  const expected = new LinkedList([]);
-  expect(actual).toEqual(expected.head);
-});
+    describe(`${name}`, () => {
 
-test('remove the actually first node', () => {
-  const linkedList = new LinkedList([1, 2, 3, 4, 5]);
-  const n = 5;
-  const actual = removeNthFromEnd(linkedList.head, n);
-  const expected = new LinkedList([2, 3, 4, 5]);
-  expect(actual).toEqual(expected.head);
-});
+      test('normal case', () => {
+        const linkedList = new LinkedList([1, 2, 3, 4, 5]);
+        const n = 2;
+        const actual = removeNthFromEnd(linkedList.head, n);
+        const expected = new LinkedList([1, 2, 3, 5]);
+        expect(actual).toEqual(expected.head);
+      });
+
+      test('remove the only node', () => {
+        const linkedList = new LinkedList([1]);
+        const n = 1;
+        const actual = removeNthFromEnd(linkedList.head, n);
+        const expected = new LinkedList([]);
+        expect(actual).toEqual(expected.head);
+      });
+
+      test('remove the actually first node', () => {
+        const linkedList = new LinkedList([1, 2, 3, 4, 5]);
+        const n = 5;
+        const actual = removeNthFromEnd(linkedList.head, n);
+        const expected = new LinkedList([2, 3, 4, 5]);
+        expect(actual).toEqual(expected.head);
+      });
+    });
+  });
+
