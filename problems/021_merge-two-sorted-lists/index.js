e746b87dfbd105862c031baf30e3244d6e5fef46
Jizu Sun <sunjizu@gmail.com>
2019-05-18 15:54:04 +0800

    easy | merge two sorted lists | recursive, linked list

--- problems/021_merge-two-sorted-lists/index.js
+++ problems/021_merge-two-sorted-lists/index.js
@@ -14,6 +14,7 @@ const {ListNode} = require('../../lib/LinkedList');
  */
 var mergeTwoLists = function(l1, l2) {
   let dummyHead = new ListNode();
+  dummyHead.tail = null;
   let tail = dummyHead;
   while (l1!==null && l2!==null) {
     if (l1.val < l2.val)  {
@@ -24,13 +25,26 @@ var mergeTwoLists = function(l1, l2) {
       l2 = l2.next;
     }
     tail = tail.next;
+    tail.next = null;
   }
-  if (l1 === null)  {
-    tail.next = l2;
-  } else {
-    tail.next = l1; 
-  }
+  tail.next = l1 || l2;
   return dummyHead.next;
 };
 
-module.exports = mergeTwoLists;
+var mergeTwoListsRecursive = function(l1, l2) {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  if (l1.val > l2.val ) {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  } else {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  }
+};
+
+module.exports = {
+  mergeTwoLists,
+  mergeTwoListsRecursive
+};
--- problems/021_merge-two-sorted-lists/index.test.js
+++ problems/021_merge-two-sorted-lists/index.test.js
@@ -1,27 +1,36 @@
-const mergeTwoLists = require('./index');
-const {LinkedList} = require('../../lib/LinkedList');
+// const mergeTwoLists = require('./index');
+// const {LinkedList} = require('../../lib/LinkedList');
 
+const {LinkedList, run} = require('lib');
+const index = require('./index');
 
-it('normal case', ()=> {
-  const l1 = new LinkedList([1, 2, 4]);
-  const l2 = new LinkedList([1, 3, 4]);
-  const actual = mergeTwoLists(l1.head, l2.head);
-  const expected = new LinkedList([1, 1, 2, 3, 4, 4]);
-  expect(actual).toEqual(expected.head);
-});
+const runTests = (mergeTwoLists) => {
 
-it('both of the linked lists is empty', ()=> {
-  const l1 = new LinkedList([]);
-  const l2 = new LinkedList([]);
-  const actual = mergeTwoLists(l1.head, l2.head);
-  const expected = new LinkedList([]);
-  expect(actual).toEqual(expected.head);
-});
+  it('normal case', ()=> {
+    const l1 = new LinkedList([1, 2, 4]);
+    const l2 = new LinkedList([1, 3, 4]);
+    const actual = mergeTwoLists(l1.head, l2.head);
+    const expected = new LinkedList([1, 1, 2, 3, 4, 4]);
+    expect(actual).toEqual(expected.head);
+  });
 
-it('one linked list is greater than another', ()=> {
-  const l1 = new LinkedList([1, 2, 3]);
-  const l2 = new LinkedList([8, 9]);
-  const actual = mergeTwoLists(l1.head, l2.head);
-  const expected = new LinkedList([1, 2, 3, 8, 9]);
-  expect(actual).toEqual(expected.head);
-});
+  it('both of the linked lists is empty', ()=> {
+    const l1 = new LinkedList([]);
+    const l2 = new LinkedList([]);
+    const actual = mergeTwoLists(l1.head, l2.head);
+    const expected = new LinkedList([]);
+    expect(actual).toEqual(expected.head);
+  });
+
+  it('one linked list is greater than another', ()=> {
+    const l1 = new LinkedList([1, 2, 3]);
+    const l2 = new LinkedList([8, 9]);
+    const actual = mergeTwoLists(l1.head, l2.head);
+    const expected = new LinkedList([1, 2, 3, 8, 9]);
+    expect(actual).toEqual(expected.head);
+  });
+
+};
+
+run(index, runTests);
+
